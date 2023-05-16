a762e1612373651ec91efecb3a1b7495a38fcbdc
Effanville <>
2023-05-16 20:43:02 +0800

    Make portfolioUpdater generic

--- FinancialStructures/Database/PortfolioUpdater.cs
+++ FinancialStructures/Database/PortfolioUpdater.cs
@@ -4,39 +4,39 @@ using Common.Structure.DataStructures;
 namespace FinancialStructures.Database
 {
     /// <summary>
-    /// Implementation of a <see cref="IPortfolioUpdater"/> that performs the action asyncronously.
+    /// Implementation of a <see cref="IDatabaseUpdater{IPortfolio}"/> that performs the action asyncronously.
     /// </summary>
-    public sealed class BackgroundPortfolioUpdater : IPortfolioUpdater
+    public sealed class BackgroundPortfolioUpdater : IDatabaseUpdater<IPortfolio>
     {
         private TaskQueue _taskQueue = new TaskQueue();
 
         /// <inheritdoc/>
-        public void PerformPortfolioAction(Action<IPortfolio> action, IPortfolio portfolio)
+        public void PerformUpdateAction(Action<IPortfolio> action, IPortfolio portfolio)
         {
             _taskQueue.Enqueue(action, portfolio);
         }
     }
 
     /// <summary>
-    /// Implementation of a <see cref="IPortfolioUpdater"/> that performs the action syncronously.
+    /// Implementation of a <see cref="IDatabaseUpdater{IPortfolio}"/> that performs the action syncronously.
     /// </summary>
-    public sealed class SynchronousPortfolioUpdater : IPortfolioUpdater
+    public sealed class SynchronousPortfolioUpdater : IDatabaseUpdater<IPortfolio>
     {
         /// <inheritdoc/>
-        public void PerformPortfolioAction(Action<IPortfolio> action, IPortfolio portfolio)
+        public void PerformUpdateAction(Action<IPortfolio> action, IPortfolio portfolio)
         {
             action(portfolio);
         }
     }
 
     /// <summary>
-    /// Contains methods for updating a portfolio.
+    /// Contains methods for updating an object of type <see cref="T"/> using callbacks.
     /// </summary>
-    public interface IPortfolioUpdater
+    public interface IDatabaseUpdater<T> where T : class
     {
         /// <summary>
         /// Update the portfolio with the given action.
         /// </summary>
-        void PerformPortfolioAction(Action<IPortfolio> action, IPortfolio portfolio);
+        void PerformUpdateAction(Action<T> action, T portfolio);
     }
 }
